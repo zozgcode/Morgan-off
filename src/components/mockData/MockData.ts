89ca5bdf3bfea0789edea89418e1ac2bfc5e0b05
sunshine-web-coder <webdev4gaming@gmail.com>
2024-06-13 00:41:26 +0100

    m

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -30,4 +30,33 @@ export const mockAccounts: Account[] = [
       },
     ],
   },
+  {
+    account_id: 'ACC002',
+    holder: {
+      firstName: 'Leslie',
+      lastName: 'Ribey',
+      jointNames: '',
+      username: 'LesRi228',
+      password: 'Thankfules',
+    },
+    bank_details: {
+      bank_name: 'Bank of Springfield',
+      branch: 'Main Branch',
+      account_number: '23232932732',
+      routing_number: '623623636',
+      swift_code: 'BOSUS33',
+      account_type: 'Checking',
+      balance_usd: 800000000,
+    },
+    transaction_history: [
+      {
+        transaction_id: 'TXN001',
+        dateTime: 'June 13, 2024 - 3:32:32 am',
+        description: "Deposit - INHERITANCE",
+        status: "Success",
+        amount_usd: 800000000,
+        account_no: "****5009",
+      },
+    ],
+  },
 ];
